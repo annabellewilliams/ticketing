ec095cc439ed44f914d3ca458f76aba8489c299d
annabellewilliams <annabellewilliams@users.noreply.github.com>
2024-01-08 16:38:04 -0500

    Edits start up log

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,7 @@ import { TicketCreatedListener } from "./events/listeners/ticket-created-listene
 import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 
 const start = async () => {
-    console.log('Starting...');
+    console.log('Starting server...');
 
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
